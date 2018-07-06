b09b902809608c04b6cc05c32b3330156ad7670e
Viacheslav Makoveichuk <vyma@ciklum.net>
2018-07-06 11:19:41 +0300

    Fix AssemblyInfo

--- FTSearchNet/FTSearchNet/Properties/AssemblyInfo.cs
+++ FTSearchNet/FTSearchNet/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FTSearchNet")]
+[assembly: AssemblyTitle("BH.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FTSearchNet")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("BH.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
