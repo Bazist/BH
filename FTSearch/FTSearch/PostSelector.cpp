aafac1e57fcbc7f850b17dec5d0e07bae0ea50f4
Viacheslav Makoveichuk <vmakoveichuk@deloitte.com>
2021-02-11 10:04:44 +0200

    fix solution

--- FTSearch/FTSearch/PostSelector.cpp
+++
@@ -1,7 +0,0 @@
-#include "StdAfx.h"
-#include "PostSelector.h"
-
-Selector* PostSelector::Selectors[MAX_SHORT];
-char* PostSelector::Words[MAX_SHORT];
-
-uint32 PostSelector::CountWords = 0;
--- FTSearch/FTSearch/PostSelectorPool.cpp
+++
@@ -1,9 +0,0 @@
-#include "StdAfx.h"
-#include "PostSelectorPool.h"
-
-PostSelector** PostSelectorPool::pFreePostSelectorLists[MAX_POOL_PAGES];
-PostSelector* PostSelectorPool::pPostSelectorLists[MAX_POOL_PAGES];
-
-uint PostSelectorPool::Count = 0;
-uint PostSelectorPool::Size = 0;
-char* PostSelectorPool::pLastErrorMessage = 0;
